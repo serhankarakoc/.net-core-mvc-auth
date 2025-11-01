44646ac2c7f3b98ca72a45d3e0e077d68476ab8e
Serhan KARAKOÇ <108010261+serhankarakoc@users.noreply.github.com>
2025-11-01 20:49:44 +0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BilginetAkademi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
