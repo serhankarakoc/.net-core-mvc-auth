2725750d3bd34d095fb3fccb4ee8f1f5d976ec59
Serhan KARAKOÇ <108010261+serhankarakoc@users.noreply.github.com>
2025-11-01 20:49:33 +0300

    Delete Data directory

--- Data/Seeders/ISeeder.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BilginetAkademi.Data.Seeders
-{
-    // Tüm seeder sınıflarının uygulayacağı arayüz
-    public interface ISeeder
-    {
-        // Seeding işlemini asenkron olarak gerçekleştirir
-        Task SeedAsync();
-    }
-}
